8ed3fcd0bd581a0a70eac5f0b447bbe8b9d4d351
c00lkidd9 <95196141+c00lkidd9@users.noreply.github.com>
2021-12-02 12:02:56 -0800

    Update pricing.js

--- src/components/Home/pricing.js
+++ src/components/Home/pricing.js
@@ -13,26 +13,23 @@ const PricingMain = () => {
                 Unlimited access to all<br></br>products.
               </h1>
               <p className="mt-5 text-sm opacity-50 font-semibold">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+               this is another placeholder
               </p>
               <div className="mt-5">
-                <h2 className="font-bold text-xl">What's Included</h2>
+                <h2 className="font-bold text-xl">give me money</h2>
                 <div className="mt-5 grid grid-cols-2 gap-3">
-                  <h3>Feature One</h3>
-                  <h3>Feature Two</h3>
-                  <h3>Feature Three</h3>
-                  <h3>Feature Four</h3>
+                  <h3>money1</h3>
+                  <h3>money2</h3>
+                  <h3>money3</h3>
+                  <h3>money4</h3>
                 </div>
               </div>
             </div>
             <div className="flex flex-col items-center justify-center w-2/5 p-8 bg-pink rounded-r-lg">
               <h2 className="text-xl text-white text-bold">
-                All updates for one year
+                ill help for a year
               </h2>
-              <h1 className="text-7xl text-white font-bold mt-2">$149</h1>
+              <h1 className="text-7xl text-white font-bold mt-2">50 bucks</h1>
               <div className="mt-5">
                 <Button
                   colorClass="bg-white"
